8ca6dc56785d0f57a34f5e1e46e05f626a329ddf
RamSailopal <thelinuxguys@yahoo.com>
2021-09-10 14:05:48 +0100

    Have made changes to add multiple tests and actions

--- testrun.py
+++ testrun.py
@@ -6,7 +6,6 @@ import sys
 import datetime
 HTML=0
 finres=0
-cnt=1
 dat = datetime.datetime.now()
 options, remainder = getopt.getopt(sys.argv[1:], 'f:o', ['file=','output'])
 if len(options)==0:
@@ -20,27 +19,38 @@ for opt,arg in options:
       HTML=1
 if (HTML==1):
    e = open("results.html", "w")
-   e.write("<HTML>\n<BODY>\n<H1>YottaDB Test Results - " + dat.strftime("%c") + "</H1>\n<TABLE>\n<TR><TH align=\"center\">Test No</TH><TH align=\"center\">Global Entry</TH><TH align=\"center\">Actual</TH><TH align=\"center\">Expected</TH><TH align=\"center\">Result</TH></TR>\n")
+   e.write("<HTML>\n<BODY>\n<H1>YottaDB Test Results - " + dat.strftime("%c") + "</H1>\n<TABLE>\n<TR><TH align=\"center\">Test Name</TH><TH align=\"center\">Test No</TH><TH align=\"center\">Global Entry</TH><TH align=\"center\">Actual</TH><TH align=\"center\">Expected</TH><TH align=\"center\">Result</TH></TR>\n")
 with open(file) as f:
   data = json.load(f)
 for i in data:
-   cmd="ydb <<< 'W " + i["global"] + "' | awk '/(^$)|(^YDB>)/ { next } { print }'"
-   process = subprocess.Popen(cmd,
+   for j in i["tests"]:
+      cnt=0
+      print("Executing test - " + j["name"])
+      cmd=j["action"]
+      process = subprocess.Popen(cmd,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE,
                               shell=True)
-   result = process.communicate()
-   result1=result[0].replace("\n","")
-   if (str(result1) == str(i["value"])):
-      cnt=cnt+1
-      print("Actual:  " + str(result1) + "  Expected:  " + str(i["value"]) + "  ............... " + "PASS")
-      if(HTML==1):
-         e.write("<TR><TD align=\"center\">" + str(cnt) + "</TD><TD align=\"center\">" + str(i["global"]) + "</TD><TD align=\"center\">" + str(result1) + "</TD><TD align=\"center\">" + str(i["value"]) + "</TD><TD><FONT COLOR=\"#58D68D\">PASS</FONT></TD></TR>\n")
-   else:
-      finres=1
-      print("Actual:  " + str(result1) + "  Expected:  " + str(i["value"]) + " ............... " + "FAIL")
-      if(HTML==1):
-         e.write("<TR><TD align=\"center\">" + str(cnt) + "</TD><TD align=\"center\">" + str(i["global"]) + "</TD><TD align=\"center\">" + str(result1) + "</TD><TD align=\"center\">" + str(i["value"]) + "</TD><TD><FONT COLOR=\"red\">FAIL</FONT></TD></TR>\n")
+      result = process.communicate()
+      for k in j["result"]:
+         print("\nChecking global entry - " + k["global"])
+         cmd="ydb <<< 'W $G(" + k["global"] + ")' | awk '/(^$)|(^YDB>)/ { next } { print }'"
+         process = subprocess.Popen(cmd,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              shell=True)
+         result = process.communicate()
+         result1=result[0].replace("\n","")
+         if (str(result1) == str(k["value"])):
+            cnt=cnt+1
+            print("Actual:  " + str(result1) + "  Expected:  " + str(k["value"]) + "  ............... " + "PASS")
+            if(HTML==1):
+               e.write("<TR><TD align=\"center\">" +  j["name"]  + "</TD><TD align=\"center\">" + str(cnt) + "</TD><TD align=\"center\">" + str(k["global"]) + "</TD><TD align=\"center\">" + str(result1) + "</TD><TD align=\"center\">" + str(k["value"]) + "</TD><TD><FONT COLOR=\"#58D68D\">PASS</FONT></TD></TR>\n")
+         else:
+            finres=1
+            print("Actual:  " + str(result1) + "  Expected:  " + str(k["value"]) + " ............... " + "FAIL")
+            if(HTML==1):
+               e.write("<TR><TD align=\"center\">" + str(cnt) + "</TD><TD align=\"center\">" + str(k["global"]) + "</TD><TD align=\"center\">" + str(result1) + "</TD><TD align=\"center\">" + str(k["value"]) + "</TD><TD><FONT COLOR=\"red\">FAIL</FONT></TD></TR>\n")
 f.close()
 if (finres==0):
    finres1="<FONT COLOR=\"#58D68D\">PASS</FONT>"
